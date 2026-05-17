81b30f1b76bdcc7f5ea370fbacf3d9b3f574bca6
Mayur Sahare <mayursahare2024@gmail.com>
2026-05-17 22:15:58 +0530

    Configured backend for Vercel

--- server/server.js
+++ server/server.js
@@ -42,4 +42,4 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/fashion_s
   .catch(err => console.error(err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
